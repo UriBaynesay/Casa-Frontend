6d357e297c1d445dac79fc19330cab827992fbbf
Uri Baynesay <uri552@gmail.com>
2022-07-14 10:42:48 +0300

    stay review separated to cmps

---
+++ src/cmps/stay-review-scores.jsx
@@ -0,0 +1,44 @@
+import starIcon from "../assets/img/svgs/star.svg"
+
+export const StayReviewScore = ({ reviewScores,reviews }) => {
+  return (
+    <div className="reviews-score">
+      <h1>
+        <img width="14px" src={starIcon} alt="" />{" "}
+        {(reviewScores.rating / 100).toFixed(1) * 5} · {reviews.length} reviews
+      </h1>
+      <div className="rating-breakdown">
+        <h4 className="cleanliness">Cleanliness </h4>
+        <div className="meter-container c-review">
+          <meter max="5" value={reviewScores.value / 2}></meter>
+          <span>{reviewScores.cleanliness / 2}</span>
+        </div>
+        <h4 className="communication">Communication </h4>
+        <div className="meter-container communi-review">
+          <meter max="5" value={reviewScores.value / 2}></meter>
+          <span>{reviewScores.communication / 2}</span>
+        </div>
+        <h4 className="check-in">Check-in </h4>
+        <div className="meter-container checkin-review">
+          <meter max="5" value={reviewScores.value / 2}></meter>
+          <span>{reviewScores.checkin / 2}</span>
+        </div>
+        <h4 className="accuracy">Accuracy </h4>
+        <div className="meter-container acc-review">
+          <meter max="5" value={reviewScores.value / 2}></meter>
+          <span>{reviewScores.accuracy / 2}</span>
+        </div>
+        <h4 className="location">Location </h4>
+        <div className="meter-container loc-review">
+          <meter max="5" value={reviewScores.value / 2}></meter>
+          <span>{reviewScores.location / 2}</span>
+        </div>
+        <h4 className="value">Value </h4>
+        <div className="meter-container val-review">
+          <meter max="5" value={reviewScores.value / 2}></meter>
+          <span>{reviewScores.value / 2}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
