c7e975ca4ce5b3554b7c5f91f2404c7cf1b9ad56
Uri Baynesay <uri552@gmail.com>
2024-08-28 09:37:57 +0300

    fetched top rated

--- src/cmps/homepage-cmps/top-rated.jsx
+++ src/cmps/homepage-cmps/top-rated.jsx
@@ -1,18 +1,26 @@
+import { useEffect, useState } from "react"
 import { stayService } from "../../services/stay.service"
 import { TopRatedList } from "./top-rated-list"
 
 export const TopRated = () => {
-  const stays = stayService.getTopRated()
+  const [stays, setStays] = useState()
+
+  useEffect(() => {
+    fetchTopStays()
+  }, [])
+
+  const fetchTopStays = async () => {
+    const fetchedStays = await stayService.getTopRated()
+    setStays(fetchedStays)
+  }
 
   return (
     <section className="top-rated main-layout">
-      {stays ? (
+      {stays && (
         <div className="top-rated-container">
           <h1 className="title">Top Rated</h1>
           <TopRatedList stays={stays} />
         </div>
-      ) : (
-        <div></div>
       )}
     </section>
   )
